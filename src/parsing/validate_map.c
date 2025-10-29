6b124ffb117cd817d21cd8a4ae3672d26f22b94b
Moritz Knoll <mknoll@k2r2p1.42mulhouse.fr>
2025-10-29 13:48:08 +0100

    next try

--- src/parsing/validate_map.c
+++ src/parsing/validate_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validate_map.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: radubos <radubos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 15:30:00 by radubos           #+#    #+#             */
-/*   Updated: 2025/10/29 13:31:35 by radubos          ###   ########.fr       */
+/*   Updated: 2025/10/29 13:47:17 by mknoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,18 @@ int	validate_map(t_map *map)
 	if (!map->grid)
 		return (error_exit("No map data"), 1);
 	player_count = 0;
-	i = 0;
-	while (i < map->height)
+	i = -1;
+	while (++i < map->height)
 	{
-		j = 0;
-		while (j < map->width && map->grid[i][j])
+		j = -1;
+		while (++j < map->width && map->grid[i][j])
 		{
 			if (!is_valid_map_char(map->grid[i][j]))
 				return (error_exit("Invalid character in map"), 1);
 			if (map->grid[i][j] == 'N' || map->grid[i][j] == 'S' ||
 				map->grid[i][j] == 'E' || map->grid[i][j] == 'W')
 				player_count++;
-			j++;
 		}
-		i++;
 	}
 	if (player_count != 1)
 		return (error_exit("Map must contain exactly one player"), 1);
